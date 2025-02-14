bc2e8b42bf8ece0ddb133ed755bb466fdc342c0a
Thapelo Somo <edwardthapelo55@gmail.com>
2025-02-14 15:22:18 +0200

    Added endpoints to add, get, update, delete, getDeleted Trainee without the logged in user auth

--- routes/authCheck.js
+++ routes/authCheck.js
@@ -1,8 +1,15 @@
 import express from "express";
 import { verifyToken } from "../utilities/index.js";
+import { create_user, delete_User, update_User, deleted_Users, get_Users } from "../controllers/userController.js";
+
 
 const router = express.Router();
 
-router.get("/create-user",verifyToken, ()=>{console.log("user created")});
+router.get("/create-user",verifyToken, ()=>{console.log("user created")}); //Example
+router.get("/trainees", get_Users);
+router.post("/trainees", create_user);
+router.put("/trainees/:id",verifyToken, update_User);
+router.delete("/trainees/:id",verifyToken, delete_User);
+router.get("/deletedTrainees",verifyToken, deleted_Users);
 
 export default router;
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import admin from "firebase-admin";
-import serviceAccount from "./config/serviceAccountKey.json" assert { type: "json" };
+// import serviceAccount from "./config/serviceAccountKey"
 
 import authRoutes from "./routes/index.js"
 import authCheck from "./routes/authCheck.js"
@@ -10,15 +10,15 @@ import authCheck from "./routes/authCheck.js"
 const PORT = process.env.PORT;
 const app = express();
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+// });
 
 app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth/", authRoutes);
-app.use("/api/add-user/", authCheck);
+app.use("/api/", authCheck);
 
 app.all("*", (req, res) => res.send("error 404 page not found"));
 
