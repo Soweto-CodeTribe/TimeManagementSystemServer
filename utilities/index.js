ba98ec77d214f3310c815f53340c39d97226569b
MandlakheM <142303272+MandlakheM@users.noreply.github.com>
2025-02-14 11:12:24 +0200

    initialize project structure with basic authentication and middleware setup
    ng mandi branch with the authentication
    :w

--- utilities/index.js
+++ utilities/index.js
@@ -1,2 +1,25 @@
-console.log("object")
+import jwt from "jsonwebtoken";
+import secreteKey from "../config/jwtConfig.js";
 
+const generateToken = (id) => {
+  return jwt.sign({ id: id }, secreteKey, { expiresIn: "1h" });
+};
+
+export const verifyToken = async (req, res, next) => {
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      token = req.headers.authorization.split(" ")[1];
+      jwt.verify(token, secreteKey);
+
+      next();
+    } catch (error) {
+      res.status(401).json({ error: "Token is not valid" });
+    }
+  }
+};
+
+export default generateToken;
