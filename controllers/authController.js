e32d85778f13de6e5526f4ba59f3046b0ad20fee
Thapelo Somo <edwardthapelo55@gmail.com>
2025-02-17 11:07:25 +0200

    Added a Login for Trainee

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,7 +2,29 @@ import { auth } from "../config/firebaseConfig.js";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import generateToken from "../utilities/index.js";
 
+// const { token } = response.data;
+
+//Admin Login Cred
 export const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const userCredential = await signInWithEmailAndPassword(
+      auth,
+      email,
+      password,
+    );
+
+    const token = generateToken(userCredential.user.uid);
+    console.log("token", token);
+    res.status(200).json({ token: token, user: userCredential.user.email });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// Trainee Login Cred
+export const login_Trainee = async (req, res) => {
   const { email, password } = req.body;
 
   try {
