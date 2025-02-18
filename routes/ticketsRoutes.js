8607db008cba1d0b1370095d5d8f8e060d7885a5
Comfort Ngwenya <nqobilecomfyngwenya@gmail.com>
2025-02-18 09:48:58 +0200

    Adding the tickets logic for trainee and facilitators

---
+++ routes/ticketsRoutes.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import { authenticateUser } from '../middleware/auth.js';
+import { verifyToken } from "../utilities/index.js";
+import {
+  getAllTickets,
+  getTicketById,
+  updateTicket
+} from '../controllers/ticketManageController.js';
+import {
+    createTicket,
+    getMyTickets,
+    getMyTicketById,
+    updateMyTicket,
+    cancelMyTicket
+  } from '../controllers/ticketTrainee.js';
+
+const router = express.Router();
+
+// Facilitator ticket management routes
+router.get('/', verifyToken, getAllTickets);
+router.get('/:id', verifyToken, getTicketById);
+router.put('/:id', verifyToken, updateTicket);
+
+
+// Trainee ticket routes
+router.post('/', verifyToken, createTicket);
+router.get('/my-tickets', verifyToken, getMyTickets);
+router.get('/my-tickets/:id', verifyToken, getMyTicketById);
+router.put('/my-tickets/:id', verifyToken, updateMyTicket);
+router.post('/my-tickets/:id/cancel', verifyToken, cancelMyTicket);
+
+export default router;
