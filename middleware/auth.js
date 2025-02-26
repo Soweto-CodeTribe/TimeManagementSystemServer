e0e1cfc26b70eedd9f3c41e1c47157ba58189a3e
Siphelele <sphllzulu@gmail.com>
2025-02-26 15:58:26 +0200

    created the middleware for facilitator to track trainees from their own location

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,7 @@
 // import admin from 'firebase-admin';
 // import Facilitator from '../models/facilitatorModels.js';
 import { db } from '../config/firebaseConfig.js';
-import { collection, query, where, getDocs } from 'firebase/firestore';
+import { collection, query, where, getDocs,getDoc } from 'firebase/firestore';
 
 
 //Middleware to check if user has super_admin privileges
@@ -30,6 +30,47 @@ export const isSuperAdmin = async (req, res, next) => {
     }
 
     next();
+  } catch (error) {
+    console.error('Super admin check error:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+};
+
+
+export const isFacilitatorLocationTrainee = async (req, res, next) => {
+  try {
+    // Ensure req.user exists
+    if (!req.user || !req.user.uid) {
+      return res.status(401).json({ error: 'Unauthorized: No user found' });
+    }
+
+    // Check Firestore for user role
+    const facilitatorRef = doc(db, 'facilitators', req.user.uid);
+            const facilitatorDoc = await getDoc(facilitatorRef);
+    
+            if (!facilitatorDoc.exists()) {
+                return res.status(403).json({ error: 'Unauthorized: Not a facilitator' });
+            }
+    
+            const facilitatorLocation = facilitatorDoc.data().location;
+            console.log("facilitator location: ", facilitatorLocation)
+    
+            if (!facilitatorLocation) {
+                return res.status(400).json({ error: 'Facilitator location not set' });
+            }
+    
+            // Query trainees collection with location filter
+            const traineesRef = collection(db, "trainees");
+            const locationQuery = query(traineesRef, where("location", "==", facilitatorLocation));
+            const snapshot = await getDocs(locationQuery);
+    
+            const trainees = snapshot.docs.map((doc) => ({
+                id: doc.id,
+                ...doc.data(),
+            }));
+    
+        next();
+        
   } catch (error) {
     console.error('Super admin check error:', error);
     res.status(500).json({ error: 'Internal server error' });
