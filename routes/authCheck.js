2d43def9660ef0a6498a57282f811bc7c241fd35
Siphelele <sphllzulu@gmail.com>
2025-03-11 11:05:15 +0200

    uncommented the isSuperAdmin middleware

--- routes/authCheck.js
+++ routes/authCheck.js
@@ -11,7 +11,7 @@ router.get("/create-user",verifyToken, ()=>{console.log("user created")});
 
 router.get("/trainees", 
     verifyToken, 
-    // isSuperAdmin, 
+    isSuperAdmin,
     get_Users);
 
 router.get("/my-trainees", verifyToken, get_Users_By_Location);
