3fb019f8f2957ea5ed6e07b95594996ef15359ce
Comfort Ngwenya <nqobilecomfyngwenya@gmail.com>
2025-03-13 10:48:59 +0200

    Adding delete ticket

--- routes/ticketsRoutes.js
+++ routes/ticketsRoutes.js
@@ -4,14 +4,16 @@ import { verifyToken } from "../utilities/index.js";
 import {
   getAllTickets,
   getTicketById,
-  updateTicket
+  updateTicket,
+  deleteTicket
 } from '../controllers/ticketManageController.js';
 import {
     createTicket,
     getMyTickets,
     getMyTicketById,
     updateMyTicket,
-    cancelMyTicket
+    cancelMyTicket,
+    deleteMyTicket
   } from '../controllers/ticketTrainee.js';
 
 const router = express.Router();
@@ -20,7 +22,7 @@ const router = express.Router();
 router.get('/', verifyToken, getAllTickets);
 router.get('/:id', verifyToken, getTicketById);
 router.put('/:id', verifyToken, updateTicket);
-
+router.delete('/:id', verifyToken, deleteTicket);
 
 // Trainee ticket routes
 router.post('/', verifyToken, createTicket);
@@ -28,5 +30,6 @@ router.get('/my-tickets', verifyToken, getMyTickets);
 router.get('/my-tickets/:id', verifyToken, getMyTicketById);
 router.put('/my-tickets/:id', verifyToken, updateMyTicket);
 router.post('/my-tickets/:id/cancel', verifyToken, cancelMyTicket);
+router.delete('/my-tickets/:id', verifyToken, deleteMyTicket);
 
 export default router;
