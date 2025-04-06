d5bbe224e096a2085daa599a77be613ea5c0dd4c
Siphelele <sphllzulu@gmail.com>
2025-04-06 18:15:03 +0200

    Fix QR code generation schedule time to 6:30 PM

--- controllers/qrCodeController.js
+++ controllers/qrCodeController.js
@@ -92,7 +92,7 @@ export function scheduleQRCodeGeneration() {
   const now = new Date();
   const next1AM = new Date(now);
 
-  next1AM.setHours(6, 30, 0, 0); // Set time to 1:00 AM
+  next1AM.setHours(18, 30, 0, 0); // Set time to 1:00 AM
   if (next1AM < now) {
     next1AM.setDate(next1AM.getDate() + 1); // Move to next day if it's past 1 AM
   }
